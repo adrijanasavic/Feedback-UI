4a254dfac2d16d6300214399429fb563d2edc341
Adrijana <adrijana.jovicic@gmail.com>
2022-01-26 17:01:20 +0100

    used operator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-    
+
     const title = 'Blog Post';
     const body = 'This is my blog post';
     const comment = [
@@ -8,19 +8,28 @@ function App() {
         { id: 3, text: 'Comment three' }
     ];
 
+    const loading = false;
+    const showComment = true;
+
+    if (loading) return <h1>Loading...</h1>
+
+    const commentBlock = (<div className="comments">
+        <h3>Comments ({comment.length})</h3>
+        <ul>
+            {comment.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+    </div>)
+
+
     return (
         <div className="container">
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
-            {Math.random() * 5 + 5}
-            <div className="comments">
-                <h3>Comments ({comment.length})</h3>
-                <ul>
-                    {comment.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
+
+            {showComment && commentBlock}
+
         </div>
     )
 }

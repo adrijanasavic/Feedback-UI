7e3b3d909644ff5377e3d91ab68721045e57b575
Adrijana <adrijana.jovicic@gmail.com>
2022-01-27 15:12:24 +0100

    Fade Animation With Framer Motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,40 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
 function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.lenght === 0) {
-        return<p>No Feedback Yet</p>
+        return <p>No Feedback Yet</p>
     }
-        
+
     return <div className='feedback-list'>
-        {feedback.map((item) => (
-            <FeedbackItem
-                key={item.id}
-                item={item}
-                handleDelete = {handleDelete}
-            />
-      ))}
-  </div>;
+        <AnimatePresence>
+            {feedback.map((item) => (
+                <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <FeedbackItem
+                        key={item.id}
+                        item={item}
+                        handleDelete={handleDelete} />
+                </motion.div>
+            ))}
+        </AnimatePresence>
+    </div>;
+
+    // return <div className='feedback-list'>
+    //     {feedback.map((item) => (
+    //         <FeedbackItem
+    //             key={item.id}
+    //             item={item}
+    //             handleDelete={handleDelete}
+    //         />
+    //     ))}
+    // </div>;
 }
 
 FeedbackList.propTypes = {
@@ -25,7 +44,7 @@ FeedbackList.propTypes = {
             text: PropTypes.string.isRequired,
             rating: PropTypes.number.isRequired,
         })
-        
+
     )
 }
 export default FeedbackList;

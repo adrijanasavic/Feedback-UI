2974ff4d27f23c5d799bf425b9f1e5bf45ec7cb2
Adrijana <adrijana.jovicic@gmail.com>
2022-01-28 12:26:50 +0100

    Update Feedback Item

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -11,7 +11,7 @@ function FeedbackForm() {
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
 
-    const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+    const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
 
     useEffect(() => {
         if (feedbackEdit.edit === true) {
@@ -41,9 +41,14 @@ function FeedbackForm() {
         if (text.trim().length > 10) {
             const newFeedback = {
                 text,
-                rating: rating,
+                rating,
             }
-            addFeedback(newFeedback);
+            if (feedbackEdit.edit === true) {
+                updateFeedback(feedbackEdit.item.id, newFeedback)
+            } else {
+                addFeedback(newFeedback);
+
+            }
 
             setText('');
         }
